3b6c681e80061f37f84914a28d730a04a1c5f85e
Curtis Click <cdc.click@gmail.com>
2025-09-14 01:21:35 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'ge

--- src/lib/genkit.ts
+++ src/lib/genkit.ts
@@ -1,5 +1,5 @@
 'use client';
-import { Flow, runFlow } from 'genkit/flow';
+import { type Flow, runFlow } from 'genkit';
 import { z } from 'zod';
 
 export async function runGenkitFlow<I extends z.ZodType, O extends z.ZodType>(
