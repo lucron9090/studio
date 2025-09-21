5305a7a5af93f84ff46dc4aaa0cca4f71672440a
Curtis Click <cdc.click@gmail.com>
2025-09-21 05:46:21 +0000

    no responses are provided by the target in production mode

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'http://localhost:9099/:path*',
-      },
-    ];
+    if (process.env.NODE_ENV === 'development') {
+        return [
+        {
+            source: '/:path*',
+            destination: 'http://localhost:9099/:path*',
+        },
+        ];
+    }
+    return [];
   },
 };
 
