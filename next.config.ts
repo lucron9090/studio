883491e3f92578dc3aa7c3675ea5640c295e16b5
Curtis Click <cdc.click@gmail.com>
2025-09-26 03:34:23 +0000

    [GoogleGenerativeAI Error]: Error fetching from https://generativelangua

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    if (process.env.NODE_ENV === 'development') {
-        return [
-        {
-            source: '/:path*',
-            destination: 'http://localhost:9099/:path*',
-        },
-        ];
-    }
-    return [];
-  },
 };
 
 export default nextConfig;
