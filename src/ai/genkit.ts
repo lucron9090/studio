8e0e0f9ff449c9e8db3b4d6eb4c09cd1f6d6bbbe
Curtis Click <cdc.click@gmail.com>
2025-09-26 03:55:55 +0000

    vThe error (0 , _genkit_ai_google_cloud__WEBPACK_IMPORTED_MODULE_1__.goo

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,7 @@
 import {genkit} from 'genkit';
 import {googleCloud} from '@genkit-ai/google-cloud';
+import {gcp} from '@genkit-ai/gcp';
 
 export const ai = genkit({
-  plugins: [googleCloud()],
+  plugins: [googleCloud(), gcp()],
 });
