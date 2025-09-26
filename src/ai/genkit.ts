f7f8ab80f263e1e3eaff456f1e08736450589464
Curtis Click <cdc.click@gmail.com>
2025-09-26 03:45:20 +0000

    fix

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,7 @@
 import {genkit} from 'genkit';
 import {googleCloud} from '@genkit-ai/google-cloud';
-import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleCloud(), googleAI({apiKey: process.env.GEMINI_API_KEY})],
-  model: 'googleai/gemini-1.5-flash-latest',
+  plugins: [googleCloud()],
+  model: 'googleCloud/gemini-1.5-flash-latest',
 });
